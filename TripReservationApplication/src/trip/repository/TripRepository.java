43c75161df36f097eafd2ae3e5551bac787a7373
İrem Kurtulmaz <irmkrtlmz@gmail.com>
2025-06-06 20:47:27 +0300

    add trip filtering method

--- TripReservationApplication/src/trip/repository/TripRepository.java
+++ TripReservationApplication/src/trip/repository/TripRepository.java
@@ -4,6 +4,8 @@ import trip.model.Trip;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.stream.Collectors;
+import searchCriteria.TripSearchCriteria;
 
 public class TripRepository {
     private static TripRepository instance;
@@ -33,4 +35,25 @@ public class TripRepository {
         return tripList.remove(trip);
     }
 
+    public List<Trip> search(TripSearchCriteria criteria) {
+        return tripList.stream()
+
+            .filter(t -> criteria.getTripType() == null || criteria.getTripType().isInstance(t))
+
+            .filter(t -> criteria.getDepartureStation() == null || 
+                         t.getDepartureStation().equalsIgnoreCase(criteria.getDepartureStation()))
+            .filter(t -> criteria.getArrivalStation() == null || 
+                         t.getArrivalStation().equalsIgnoreCase(criteria.getArrivalStation()))
+
+            .filter(t -> criteria.getDepartureTimeFrom() == null || 
+                         !t.getDepartureDate().isBefore(criteria.getDepartureTimeFrom()))
+            .filter(t -> criteria.getDepartureTimeTo() == null || 
+                         !t.getDepartureDate().isAfter(criteria.getDepartureTimeTo()))
+
+            .filter(t -> criteria.getMinFare() == null || t.getFare() >= criteria.getMinFare())
+            .filter(t -> criteria.getMaxFare() == null || t.getFare() <= criteria.getMaxFare())
+
+            .collect(Collectors.toList());
+    }
+    
 }
